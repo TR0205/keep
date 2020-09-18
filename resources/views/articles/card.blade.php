a8637e6419e4f99969b3d7660cce231860dbbf21
TR0205 <index0205@gmail.com>
2020-09-18 15:20:13 +0900

    インデント修正

--- resources/views/articles/card.blade.php
+++ resources/views/articles/card.blade.php
@@ -5,10 +5,12 @@
     </a>
     <div>
       <div class="font-weight-bold">
-      <a href="{{ route('users.show', ['name' => $article->user->name]) }}" class="text-dark">
-        {{ $article->user->name }}</div>
-      </a>
-      <div class="font-weight-lighter">{{ $article->created_at->format('Y/m/d H:i') }}</div>
+        <a href="{{ route('users.show', ['name' => $article->user->name]) }}" class="text-dark">
+        {{ $article->user->name }}
+      </div>
+      <div class="font-weight-lighter">
+        {{ $article->created_at->format('Y/m/d H:i') }}
+      </div>
     </div>
 
   @if( Auth::id() === $article->user_id )
@@ -16,7 +18,9 @@
       <div class="ml-auto card-text">
         <div class="dropdown">
           <a data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <i class="fas fa-ellipsis-v"></i>
+            <button type="button" class="btn btn-link text-muted m-0 p-2">
+              <i class="fas fa-ellipsis-v"></i>
+            </button>
           </a>
           <div class="dropdown-menu dropdown-menu-right">
             <a class="dropdown-item" href="{{ route('articles.edit', ['article' => $article]) }}">
@@ -57,39 +61,39 @@
       <!-- modal -->
     @endif
 
+  </div>
+  <div class="card-body pt-0 pb-2">
+    <h3 class="card-title">
+      <a class="text-dark" href="{{ route('articles.show', ['article' => $article]) }}">
+        {{ $article->title }}
+      </a>
+    </h3>
+    <div class="card-text">
+      {!! nl2br(e( $article->body )) !!}
     </div>
-    <div class="card-body pt-0 pb-2">
-      <div class="card-title">
-        <a class="text-dark" href="{{ route('articles.show', ['article' => $article]) }}">
-          {{ $article->title }}
-        </a>
-      </div>
-      <div class="card-text">
-        {!! nl2br(e( $article->body )) !!}
-      </div>
-    </div>
-    <div class="card-body pt-0 pb-2 pl-3">
-      <div class="card-text">
-        <article-like
+  </div>
+  <div class="card-body pt-0 pb-2 pl-3">
+    <div class="card-text">
+      <article-like
         :initial-is-liked-by='@json($article->isLikedBy(Auth::user()))'
         :initial-count-likes='@json($article->count_likes)'
         :authorized='@json(Auth::check())'
         endpoint="{{ route('articles.like', ['article' => $article]) }}"
-        >
-        </article-like>
-      </div>
+      >
+      </article-like>
     </div>
-    @foreach($article->tags as $tag)
+  </div>
+  @foreach($article->tags as $tag)
     @if($loop->first)
       <div class="card-body pt-0 pb-4 pl-3">
         <div class="card-text line-height">
     @endif
-    <a href="{{ route('tags.show', ['name' => $tag->name]) }}" class="border p-1 mr-1 mt-1 text-muted">
-      {{ $tag->hashtag }}
-    </a>
+          <a href="{{ route('tags.show', ['name' => $tag->name]) }}" class="border p-1 mr-1 mt-1 text-muted">
+            {{ $tag->hashtag }}
+          </a>
     @if($loop->last)
         </div>
       </div>
     @endif
-    @endforeach
-  </div>
+  @endforeach
+</div>

38c0f41344b804e230c2387bd218852824b7ebc3
TR0205 <index0205@gmail.com>
2020-09-25 13:31:18 +0900

    タグランキング機能実装

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -4,22 +4,22 @@
 
 @section('content')
   @include('nav')
+  <div class="container">
     <div class="card-body text-center">
-        <h2 class="h3 card-title text-center mt-4">人気のタグ</h2>
-      </div>
-      <div class="card m-1 p-3 d-flex flex-row flex-wrap">
-        @foreach($tags as $tag)
-          <div class="card-body pt-0 pb-4 pl-3">
-            <div class="card-text line-height">
-              <a href="{{ route('tags.show', ['name' => $tag->name]) }}" class="border p-1 mr-1 mt-1 text-muted">
-                #{{ $tag->name }}
-              </a>
-            </div>
-          </div>
-        @endforeach
-      </div>
+      <h2 class="h3 card-title text-center mt-4">人気のタグ</h2>
     </div>
-
+    <div class="card mb-5 p-3 d-flex flex-row flex-wrap">
+      @foreach($tags as $tag)
+        <div class="card-body pt-0 pb-4 pl-3">
+          <div class="card-text line-height">
+            <a href="{{ route('tags.show', ['name' => $tag->name]) }}" class="border p-1 mr-1 mt-1 text-muted">
+              #{{ $tag->name }}
+            </a>
+            :{{ $tag->articles->count() }}件
+          </div>
+        </div>
+      @endforeach
+    </div>
     @foreach($articles as $article)
       @include('articles.card')
     @endforeach
